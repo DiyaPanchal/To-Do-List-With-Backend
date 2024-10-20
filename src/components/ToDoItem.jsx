aa505a32432924457183971ca35bc9fd5d1c03eb
Diya Panchal <diya.pce21@sot.pdpu.ac.in>
2024-10-20 19:21:17 +0530

    styling changes

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 function ToDoItem(props) {
+  const [isDone, setIsDone] = useState(false); 
   const [isEditing, setIsEditing] = useState(false);
   const [newText, setNewText] = useState(props.text);
 
@@ -13,29 +14,40 @@ function ToDoItem(props) {
   }
 
   function handleSaveClick() {
-    props.onEdit(props.id, newText); 
-    setIsEditing(false); 
+    props.onEdit(props.id, newText);
+    setIsEditing(false);
+  }
+
+  function handleToggleCompleted() {
+    setIsDone((prevValue) => !prevValue);
   }
 
   return (
-    <div>
+    <div className="todoitem">
       {isEditing ? (
         <input
           type="text"
           value={newText}
           onChange={handleChange}
-          className="edit-input" 
+          className="form-input"
         />
       ) : (
-        <li>{props.text}</li>
+        <li
+          onClick={handleToggleCompleted}
+          style={{ textDecoration: isDone ? "line-through" : "none" }}
+        >
+          {props.text}
+        </li>
       )}
-      <button onClick={handleEditClick}>{isEditing ? "Cancel" : "Edit"}</button>
-      {isEditing && <button onClick={handleSaveClick}>Save</button>}
-      <button
-        onClick={() => {
-          props.onChecked(props.id);
-        }}
-      >
+      <button className="list-button" onClick={handleEditClick}>
+        {isEditing ? "Cancel" : "Edit"}
+      </button>
+      {isEditing && (
+        <button className="list-button" onClick={handleSaveClick}>
+          Save
+        </button>
+      )}
+      <button className="list-button" onClick={() => props.onChecked(props.id)}>
         Delete
       </button>
     </div>
